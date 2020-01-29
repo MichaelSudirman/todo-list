59a152c611190740158885d15d9b1765f81dab0b
MichaelSudirman <astradon.michael@gmail.com>
2020-01-29 21:55:13 +0700

    remove unecessary import

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -8,7 +8,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 
 import Modal from 'react-bootstrap/Modal';
 /*
