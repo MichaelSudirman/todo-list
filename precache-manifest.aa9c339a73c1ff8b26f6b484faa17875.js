a422937120728e0171fbcd53f95500f6fb7231e2
MichaelSudirman <astradon.michael@gmail.com>
2020-01-29 18:59:03 +0700

    Updates

---
+++ precache-manifest.aa9c339a73c1ff8b26f6b484faa17875.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "960163dbc7608364bc00a4572f4706f1",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "97083e66f1c56809200a",
+    "url": "/todo/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "c9ebcbaccc79e899006c",
+    "url": "/todo/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "97083e66f1c56809200a",
+    "url": "/todo/static/js/2.2a8f9f39.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/todo/static/js/2.2a8f9f39.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c9ebcbaccc79e899006c",
+    "url": "/todo/static/js/main.012c59c2.chunk.js"
+  },
+  {
+    "revision": "0d09011ba5aec1e0a3a2",
+    "url": "/todo/static/js/runtime-main.232339c7.js"
+  }
+]);
